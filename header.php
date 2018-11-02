5fefaf7a863197a828f0599adbbfe850744042de
langletamelie <amelie.langlet@hotmail.com>
2018-11-02 09:16:55 +0100

    ajout des dossiers

---
+++ header.php
@@ -0,0 +1,12 @@
+<nav>
+    <div class="nav-wrapper">
+        <ul id="back-nav" class="hide-on-med-and-down">
+            <li><a class="blue-grey darken-4 btn-large">Page d'accueil</a></li>
+            <li><a class="blue-grey darken-4 btn-large">Chercher un atelier</a></li>
+            <li><a class="blue-grey darken-4 btn-large">Proposer un atelier</a></li>
+            <li><a class="blue-grey darken-4 btn-large">Inscription</a></li>
+            <li><a class="blue-grey darken-4 btn-large">Connexion</a></li>
+            <li><a class="blue-grey darken-4 btn-large">Mon profil</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset = "utf-8" />
+        <link rel="stylesheet" href="../assets/css/style.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">     
+        <title>PROJET PROF</title>
+    </head>
+    <body>
+          <img src="../assets/img/inspiration.png" id="classroom"/>
+        <?php include 'header.php'; ?>
+    </body>
+  
+</html>
